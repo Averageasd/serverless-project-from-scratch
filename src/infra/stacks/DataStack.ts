b6930179a22b2caef8edc7814bd507ede49750a1
Averageasd <nhuynh9497@gmail.com>
2025-08-10 23:11:01 -0400

    removed strict:true so that we can import aws sdk.

--- src/infra/stacks/DataStack.ts
+++ src/infra/stacks/DataStack.ts
@@ -1,8 +1,20 @@
-import {Stack, StackProps} from 'aws-cdk-lib';
+import { Stack, StackProps} from 'aws-cdk-lib';
+import { AttributeType, ITable, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { QueryCommand, QueryCommandInput } from '@aws-sdk/client-dynamodb';
 
 export class DataStack extends Stack {
+
+    public readonly spaceTable!: ITable;
+
     constructor(scope: Construct, id: string, props?: StackProps){
         super(scope, id, props);
+        this.spaceTable = new Table(this, 'SpacesTable',{
+            partitionKey:{
+                name: 'id',
+                type: AttributeType.STRING
+            }
+        });
     }
 }
