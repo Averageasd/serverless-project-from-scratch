b7a814969cbb5302b89e6e4f18bd9a3e4ffc394d
Nguyen Huynh <nhuynh9497@gmail.com>
2025-08-25 16:47:54 -0400

    added bucket to store photos.

--- src/infra/stacks/DataStack.ts
+++ src/infra/stacks/DataStack.ts
@@ -1,10 +1,12 @@
 import { Stack, StackProps} from 'aws-cdk-lib';
 import { AttributeType, BillingMode, ITable, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Bucket, HttpMethods, ObjectOwnership } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class DataStack extends Stack {
 
     public readonly spaceTable!: ITable;
+    public readonly photosBucket!: Bucket;
 
     constructor(scope: Construct, id: string, props?: StackProps){
         super(scope, id, props);
@@ -15,6 +17,26 @@ export class DataStack extends Stack {
             },
             billingMode: BillingMode.PAY_PER_REQUEST, // On-Demand capacity mode
 
+        });
+
+        this.photosBucket = new Bucket(this, 'SpacePhotoBucket', {
+            cors: [{
+                allowedMethods: [
+                    HttpMethods.HEAD,
+                    HttpMethods.GET,
+                    HttpMethods.PUT
+                ],
+                allowedOrigins: ['*'],
+                allowedHeaders: ['*']
+            }],
+            // accessControl: BucketAccessControl.PUBLIC_READ, // currently not working,
+            objectOwnership: ObjectOwnership.OBJECT_WRITER,
+            blockPublicAccess: {
+                blockPublicAcls: false,
+                blockPublicPolicy: false,
+                ignorePublicAcls: false,
+                restrictPublicBuckets: false
+            }
         });
     }
 }
